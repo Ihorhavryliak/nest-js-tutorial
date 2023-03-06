17e48ed7d54d078bdc7737161690499461c71923
Ihor <kachkamala@gmail.com>
2023-03-06 16:15:54 +0200

    Pipes 7

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,30 @@
 import { CreateUserDto } from './dto/user.dto';
-import { BadRequestException, Body, Controller, Get, Header, HttpCode, HttpException, HttpStatus, Param, Post, Redirect, UsePipes, ValidationPipe, ParseBoolPipe, ParseIntPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Redirect,
+  UsePipes,
+  ValidationPipe,
+  ParseBoolPipe,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ForbiddenException } from './exeption/forbidden.exaption';
 import { ValidationPipeString } from './pipe/validator.pipe';
 
-
 @Controller()
 export class AppController {
-
   @Get(':id')
   async getHello(@Param('id', ParseIntPipe) id: number) {
-    return 'Tile 1'
+    return 'Tile 1';
   }
-
- 
-
 }
 
 /* ValidationPipe
